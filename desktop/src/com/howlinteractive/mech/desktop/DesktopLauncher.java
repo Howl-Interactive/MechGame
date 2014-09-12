1eeb53595092157aca281997b00882f14db3815e
Jacob Macdonald <jacoblmacdonald@gmail.com>
2014-09-12 16:06:44 -0400

    first java commit--empty

---
+++ desktop/src/com/howlinteractive/mech/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.howlinteractive.mech.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.howlinteractive.mech.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
